7e666f80cba00ceda2c678051435762dd23e7013
Matheus William <matheuswr89@outlook.com>
2023-09-17 22:54:13 -0300

    feat: add initial web aplication

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["cdn.shopify.com"] },
-  plugins: ["react-native-reanimated/plugin"],
 };
 
 module.exports = nextConfig;
